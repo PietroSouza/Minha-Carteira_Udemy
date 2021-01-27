0a24dbc3f504dc0a3a0b411f6cb8ab41ae13ae0a
Gabriel <gabrielpietrosouza@gmail.com>
2021-01-27 09:56:23 -0300

    ContentHeader

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import ContentHeader from '../../components/ContentHeader'
+import SelectInput from '../../components/SelectInput'
+import {Container} from './styles';
 
 const Dashboard: React.FC = () => {
+
+    const options = [
+        {value: 'Eu', label: 'eu'},
+        {value: 'Ele', label:'ele'}
+    ]
+
     return(
-        <p>A</p>
+        <Container>
+            <ContentHeader title="Dashboard" lineColor="#fff">
+                <SelectInput options={options}/>
+            </ContentHeader>
+        </Container>
     );
 }
 export default Dashboard
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
+import ContentHeader from '../../components/ContentHeader'
+import SelectInput from '../../components/SelectInput'
+import {Container} from './styles'
+
 const List: React.FC = () => {
+    const options = [
+        {value: 'Eu', label: 'eu'},
+        {value: 'Ele', label:'ele'}
+    ]
+
     return(
-        <p>A</p>
+        <Container>
+            <ContentHeader title="Saídas" lineColor="#E44C4E">
+                <SelectInput options={options}/>
+            </ContentHeader>
+        </Container>
     );
 }
 export default List
