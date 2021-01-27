0b746104cbafaf548c43f1838b5213e1dbf0a2ea
Gabriel <gabrielpietrosouza@gmail.com>
2021-01-26 23:11:27 -0300

    Layout and Header

---
+++ src/pages/List/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const List: React.FC = () => {
+    return(
+        <p>A</p>
+    );
+}
+export default List
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SingIn: React.FC = () => {
+    return(
+        <p>A</p>
+    );
+}
+export default SingIn
