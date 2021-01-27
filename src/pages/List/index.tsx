6d6c49bd747ee13cbb7ca89bd4b6043b484177c6
Gabriel <gabrielpietrosouza@gmail.com>
2021-01-27 16:47:01 -0300

    List Page

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,20 +1,134 @@
-import React from 'react';
+import React, {useMemo, useState, useEffect} from 'react';
 
 import ContentHeader from '../../components/ContentHeader'
 import SelectInput from '../../components/SelectInput'
-import {Container} from './styles'
+import HistoryFinanceCard from '../../components/HistoryFinanceCard'
 
-const List: React.FC = () => {
-    const options = [
-        {value: 'Eu', label: 'eu'},
-        {value: 'Ele', label:'ele'}
-    ]
+import gains from '../../repositories/gains'
+import expenses from '../../repositories/expenses'
+import formatCurrency from '../../utils/formatCurrency'
+import formatDate from '../../utils/formatDate'
+
+import {Container, Content, Filters} from './styles'
+
+interface IRouteParams {
+    match:{
+        params:{
+            type:string;
+        }
+    }
+}
+
+interface IData{
+    id:string;
+    description: string;
+    amountFormated: string;
+    frequency: string;
+    dateFormated: string;
+    tagColor:string;
+}
+
+const List: React.FC<IRouteParams> = ({ match }) => {
+    const [data, setData] = useState<IData[]>([])
+    const [monthSelected, setMonthSelected] = useState<string>(String(new Date().getMonth()+1));
+    const [yearSelected, setYearSelected] = useState<string>(String(new Date().getFullYear()));
+
+    const {type} = match.params;
+    const list = useMemo(() => {
+        return type === 'entry-balance' ? { 
+            title: 'Entradas',
+            lineColor: '#F7931B'
+        } : {
+            title:'Saídas',
+            lineColor:'#E44C4E'
+        }
+    },[type]);
+
+    const listData = useMemo(() => {
+        return type === 'entry-balance' ? gains : expenses;
+    },[type]);
+    
+    const months = [
+        {value: 1, label:'Janeiro'},
+        {value: 2, label:'Fevereiro'}, 
+        {value: 3, label:'Março'},
+        {value: 4, label:'Abril'},
+        {value: 5, label:'Maio'},
+        {value: 6, label:'Junho'},
+        {value: 7, label: 'Julho'},
+        {value: 8, label:'Agosto'},
+        {value: 9, label:'Setembro'},
+        {value: 10, label:'Outubro'},
+        {value: 11, label:'Novembro'},
+        {value: 12, label:'Dezembro'},
+        
+    ];
+
+    const years = [
+        {value: 2018, label: 2018},
+        {value: 2019, label: 2019},
+        {value: 2020, label: 2020},
+        {value: 2021, label: 2021},
+    ];
+
+    useEffect(() => {
+        const filteredDate = listData.filter(item => {
+            const date = new Date(item.date);
+            const month = String(date.getMonth() + 1);
+            const year = String(date.getFullYear());
+
+            return month === monthSelected && year === yearSelected
+        });
+
+        const formattedDate = filteredDate.map(item => {
+            return{ 
+                id: String(new Date().getTime() + item.amount),
+                description: item.description,
+                amountFormated: formatCurrency(Number(item.amount)),
+                frequency: item.frequency,
+                dateFormated: formatDate(item.date),
+                tagColor: item.frequency === 'recorrente' ? '#4E41F0' : '#E44C4E'
+            }
+
+        })   
+        setData(formattedDate);
+    },[listData, monthSelected, yearSelected, data.length]);
 
     return(
         <Container>
-            <ContentHeader title="Saídas" lineColor="#E44C4E">
-                <SelectInput options={options}/>
+            <ContentHeader title={list.title} lineColor={list.lineColor}>
+                <SelectInput options={months} onChange={(e) => setMonthSelected(e.target.value)} defaultValue={monthSelected}/>
+                <SelectInput options={years} onChange={(e) => setYearSelected(e.target.value)} defaultValue={yearSelected}/>
             </ContentHeader>
+
+            <Filters>
+                <button 
+                    type="button"
+                    className='tag-filter tag-filter-recurrent'
+                >
+                    Recorrentes
+                </button>
+                <button 
+                    type="button"
+                    className='tag-filter tag-filter-eventual'
+                >
+                    Eventuais
+                </button>
+            </Filters>
+
+            <Content>
+               {
+                   data.map(item => (
+                    <HistoryFinanceCard 
+                        key={item.id}
+                        tagColor={item.tagColor}
+                        title={item.description}
+                        subtitle={item.dateFormated}
+                        amount={item.amountFormated}
+                    />
+                   ))  
+               } 
+            </Content>
         </Container>
     );
 }
